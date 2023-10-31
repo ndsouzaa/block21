e514634c0f9a32f2790c49216c5a18e88f952e33
Neha <nehad2000@gmail.com>
2023-10-31 14:04:20 -0500

    finished workshop

--- index.js
+++ index.js
@@ -1,33 +1,79 @@
 const COHORT = '2309-FTB-ET-WEB-FT'
-const API_URL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/${COHORT}/parties`
+const API = "https://fsa-crud-2aa9294fe819.herokuapp.com/api/" + COHORT;
 
 const state = {
-  parties: [],
+  events: []
 }
 
-const partiesList = document.querySelector('#parties');
-const addPartyForm = document.querySelector('#addParty');
+const partyList = document.getElementById("partyList");
+const partyForm = document.getElementById('partyForm');
 
-async function getParties(){
-  const response = await fetch(API_URL, {
-
-  })
-}
-
-function renderParties(){
-  partiesList.innerHTML = '';
-  state.parties.forEach(party => {
-    const partyItem = document.createElement('li');
-    partyItem.innerHTML =
-    //TODO;
-
-    partiesList.appendChild(partyItem)
-  })
-}
-async function addParty(event){
+async function createEvent(event){
   event.preventDefault();
-  //TODO
+  try{
+    const response = await fetch(API + '/events',{
+      method: "POST",
+      headers: {'Content-type': 'application/json'},
+      body: JSON.stringify({
+        name: document.getElementById('name').value,
+        description: document.getElementById('description').value,
+        date: `${document.getElementById('date').value}:00.000Z`,
+        location:document.getElementById('location').value,
+      }) 
+    });
+    getEvents();
+
+  }catch(error){
+    console.error(error);
+  }
 }
 
-//TODO post
-//TODO delete
+partyForm.addEventListener('submit', createEvent)
+
+
+
+async function getEvents(){
+  try {
+    const response = await fetch(API + "/events");
+    const json = await response.json();
+    state.events = json.data;
+    renderGetEvents()
+  } catch(error){
+    console.error(error);
+  }
+}
+
+async function deleteEvent(){
+
+}
+
+function renderGetEvents(){
+const events = state.events.map((event) =>{
+  const article = document.createElement('article');
+  const deleteBtn = document.createElement('button');
+  deleteBtn.innerHTML = "X";
+  deleteBtn.addEventListener('click', async() =>{
+    try{
+      const response = await fetch(API + `/events/${event.id}`, {
+        method: 'DELETE'
+
+      });
+      console.log('deleted')
+      getEvents();
+
+    }catch(error){
+      console.error(error);
+    }
+
+  })
+  article.innerHTML = `
+  <h3>${event.name}</h3>
+  <address>${event.location}</address>`
+  article.append(deleteBtn)
+
+  return article;
+});
+partyList.replaceChildren(...events);
+}
+
+getEvents();
